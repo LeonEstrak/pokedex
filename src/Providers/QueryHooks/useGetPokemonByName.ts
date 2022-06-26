724d42d0c9227cbda15d5603054e05509d6addcd
Aniket Chakraborty <aniket2583@gmail.com>
2022-06-26 18:23:34 +0530

    Added GraphQL deps. Created Custom hook to get Pokemon by name.
    Signed-off-by: Aniket Chakraborty <aniket2583@gmail.com>

---
+++ src/Providers/QueryHooks/useGetPokemonByName.ts
@@ -0,0 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POKEMON_BY_NAME = gql`
+  query GET_POKEMON_BY_NAME($name: String!) {
+    pokemon_v2_pokemon(where: { name: { _eq: $name } }) {
+      id
+      name
+      height
+      weight
+      base_experience
+      pokemon_species_id
+    }
+  }
+`;
+
+interface GetPokemonByNameResponseShape {
+  pokemon_v2_pokemon: {
+    id: number;
+    name: string;
+    height: number;
+    weight: number;
+    base_experience: number;
+    pokemon_species_id: number;
+  };
+}
+
+function useGetPokemonByName(name: string) {
+  return useQuery(GET_POKEMON_BY_NAME, {
+    variables: {
+      name: name,
+    },
+  });
+}
---
+++ src/Providers/config.ts
@@ -0,0 +1,6 @@
+export default class ConfigProvider {
+  static defaultHeaders = {
+    "content-type": "application/json",
+    "x-hasura-admin-secret": "pokemon",
+  };
+}
