a17c858dd5204cc5242b3d59de0a5276103b5a75
SAHASRABDI <sahasrabdi721306@gmail.com>
2022-07-03 15:18:20 +0530

    Corrected Searchbar

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -5,11 +5,11 @@ function Searchbar() {
   const [pokemonName, setPokemonName] = useState("");
   const navigate = useNavigate();
   return (
-    <div className="flex items-center">
-      <div className="relative w-full">
-        <div className="flex inset-y-0 left-0   pl-3 pointer-events-none">
+    <div className="flex px-4 items-center justify-center">
+      <div className="relative mr-3">
+        <div className="absolute left-6 bottom-2.5 items-center pointer-events-none">
           <svg
-            className="w-6 h-6 absolute text-gray-400"
+            className="w-5 h-5 absolute text-gray-400"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -22,27 +22,27 @@ function Searchbar() {
             ></path>
           </svg>
         </div>
-        <input
-          type="text"
-          className="bg-gray-700 border-gray-600 text-lg rounded-lg placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 "
-          placeholder="Search"
-          value={pokemonName}
-          onChange={(e) => setPokemonName(e.target.value)}
-          required
-        ></input>
-        <button
-          onClick={(e) => {
-            if (pokemonName.trim()) {
-              navigate("/pokemon/" + pokemonName.trim().toLowerCase());
-            } else {
-              navigate("/" + pokemonName.trim().toLowerCase());
-            }
-          }}
-          className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Search
-        </button>
       </div>
+      <input
+        type="text"
+        className="bg-gray-700 border-gray-600 text-lg rounded-lg placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 "
+        placeholder="Search"
+        value={pokemonName}
+        onChange={(e) => setPokemonName(e.target.value)}
+        required
+      ></input>
+      <button
+        onClick={(e) => {
+          if (pokemonName.trim()) {
+            navigate("/pokemon/" + pokemonName.trim().toLowerCase());
+          } else {
+            navigate("/" + pokemonName.trim().toLowerCase());
+          }
+        }}
+        className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Search
+      </button>
     </div>
   );
 }
